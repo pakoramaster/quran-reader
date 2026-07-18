7fe398c78666b26d2bb635ccb70c29af10d314ab
Hamza Tariq <hamzatariq69@gmail.com>
2026-07-18 14:51:12 -0400

    local fixes

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,6 @@
 const { getDefaultConfig } = require('expo/metro-config');
 
 const config = getDefaultConfig(__dirname);
-config.resolver.assetExts.push('sqlite');
+config.resolver.assetExts.push('sqlite', 'wasm');
 
 module.exports = config;
